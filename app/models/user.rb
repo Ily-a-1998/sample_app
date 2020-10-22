0dffa975a05b52d7cf4c371bcee28bf803e0309f
Ily-a-1998 <illlya2018@mail.ru>
2020-10-22 16:43:04 +0600

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	before_save { self.email = email.downcase } # перед сохранением пользователя, регистр email меняем на нижний | база данных сохранит запись пользователя, основанную на первом запросе, и отклонит второе сохранение за нарушение уникальности.
 	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # регулировка правильности написания email
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # регулярное выражение правильности написания email
 	validates :email, presence: true, length: { maximum: 255 }, 
 	                  format: { with: VALID_EMAIL_REGEX },
 	                  uniqueness: { case_sensitive: false } # предотвращение дублирования email
